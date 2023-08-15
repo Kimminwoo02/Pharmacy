42adef6bb7f27c047075948d21ef9b6c4403e26b
Minwoo <made_power@naver.com>
2023-08-15 19:53:19 +0900

    프로젝트 초기 설정

--- src/main/java/com/example/pharmacy/PharmacyApplication.java
+++ src/main/java/com/example/pharmacy/PharmacyApplication.java
@@ -1,9 +1,13 @@
 package com.example.pharmacy;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+
+
+@SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
 public class PharmacyApplication {
 
     public static void main(String[] args) {
